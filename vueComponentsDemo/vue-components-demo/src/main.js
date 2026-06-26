0ff6c28b5d405d40d51d47f1eb50836073ac8867
Marcelofury <marcelofury13579@gmail.com>
2026-06-26 11:23:13 +0300

    chnaged .Vue  extension to .vue  extension that vite understand

--- vueComponentsDemo/vue-components-demo/src/main.js
+++ vueComponentsDemo/vue-components-demo/src/main.js
@@ -2,8 +2,10 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import ContactUs from "./components/ContactUs.Vue"
+import ContactUs from "./components/ContactUs.vue"
 
 const app = createApp(App)
+
 app.component("contact-us", ContactUs) 
+
 app.mount('#app')
