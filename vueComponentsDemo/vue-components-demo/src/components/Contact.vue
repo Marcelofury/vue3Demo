9ff83460bde049e3af90eeeef704e600a1d62be0
Marcelofury <marcelofury13579@gmail.com>
2026-06-30 11:05:08 +0300

    props and local variable - if they have the same name ; local variable takes dominance

--- vueComponentsDemo/vue-components-demo/src/components/Contact.vue
+++ vueComponentsDemo/vue-components-demo/src/components/Contact.vue
@@ -8,8 +8,8 @@
 </template>
 <script setup>
 import { ref, defineProps } from "vue";
-        const email = ref("hello@dotnetmastery.com")
-        const props = defineProps({name:String,phone:Number,ownername:String})
+        //const email = ref("hello@dotnetmastery.com")
+        const props = defineProps({name:{type:String, required:true},phone:Number,ownername:String, email:{type: String, required: false, default: "-n/a-"}})
         
     
 </script>
