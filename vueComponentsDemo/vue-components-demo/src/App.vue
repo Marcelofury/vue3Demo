c4bab94d65ba31d02f9666fcf8ee2bbe119dafc4
Marcelofury <marcelofury13579@gmail.com>
2026-07-14 12:59:55 +0300

    composibles in action

--- vueComponentsDemo/vue-components-demo/src/App.vue
+++ vueComponentsDemo/vue-components-demo/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-Hello World
+<button @click="increment">Increment</button>
+<button @click="decrement">Decrement</button>
+<p>{{ count }}</p>
 
 </template>
 
 <script setup>
+import { useCounter} from "./composible/useCounter.js"
+
+const {count,increment,decrement} = useCounter();
 
 
 </script>
